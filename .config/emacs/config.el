cc02db2d5dc170188167948bfcc4f1f4009b963d
Derek Taylor <derek@distrotube.com>
2025-02-26 20:30:35 -0600

    Updating Emacs config.

--- .config/emacs/config.el
+++ .config/emacs/config.el
@@ -478,8 +478,6 @@
     (evil-define-key 'normal git-timemachine-mode-map (kbd "C-k") 'git-timemachine-show-next-revision)
 )
 
-(use-package magit)
-
 (use-package hl-todo
   :hook ((org-mode . hl-todo-mode)
          (prog-mode . hl-todo-mode))
@@ -570,14 +568,14 @@
 
 (eval-after-load 'org-indent '(diminish 'org-indent-mode))
 
-  (custom-set-faces
-   '(org-level-1 ((t (:inherit outline-1 :height 1.7))))
-   '(org-level-2 ((t (:inherit outline-2 :height 1.6))))
-   '(org-level-3 ((t (:inherit outline-3 :height 1.5))))
-   '(org-level-4 ((t (:inherit outline-4 :height 1.4))))
-   '(org-level-5 ((t (:inherit outline-5 :height 1.3))))
-   '(org-level-6 ((t (:inherit outline-5 :height 1.2))))
-   '(org-level-7 ((t (:inherit outline-5 :height 1.1)))))
+(custom-set-faces
+ '(org-level-1 ((t (:inherit outline-1 :height 1.7))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.6))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.5))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.4))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.3))))
+ '(org-level-6 ((t (:inherit outline-5 :height 1.2))))
+ '(org-level-7 ((t (:inherit outline-5 :height 1.1)))))
 
 (require 'org-tempo)
 
@@ -656,11 +654,6 @@
 (setq use-file-dialog nil)   ;; No file dialog
 (setq use-dialog-box nil)    ;; No dialog box
 (setq pop-up-windows nil)    ;; No popup windows
-
-;; The eshell prompt
-(setopt eshell-prompt-function 'fancy-shell)
-(setopt eshell-prompt-regexp "^[^#$\n]* [$#] ")
-(setopt eshell-highlight-prompt nil)
 
 ;; Disabling company mode in eshell, because it's annoying.
 (setq company-global-modes '(not eshell-mode))
