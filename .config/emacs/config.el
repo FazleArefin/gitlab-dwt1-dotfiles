97f81d9aaf6b6f9d2febc40df268f0f48927980b
Derek Taylor <derek@distrotube.com>
2025-03-11 18:49:08 -0500

    Adding ox-hugo to Emacs for converting Org to Hugo-flavored markdown.

--- .config/emacs/config.el
+++ .config/emacs/config.el
@@ -568,14 +568,14 @@
 
 (eval-after-load 'org-indent '(diminish 'org-indent-mode))
 
-(custom-set-faces
- '(org-level-1 ((t (:inherit outline-1 :height 1.7))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.6))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.5))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.4))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.3))))
- '(org-level-6 ((t (:inherit outline-5 :height 1.2))))
- '(org-level-7 ((t (:inherit outline-5 :height 1.1)))))
+  (custom-set-faces
+   '(org-level-1 ((t (:inherit outline-1 :height 1.7))))
+   '(org-level-2 ((t (:inherit outline-2 :height 1.6))))
+   '(org-level-3 ((t (:inherit outline-3 :height 1.5))))
+   '(org-level-4 ((t (:inherit outline-4 :height 1.4))))
+   '(org-level-5 ((t (:inherit outline-5 :height 1.3))))
+   '(org-level-6 ((t (:inherit outline-5 :height 1.2))))
+   '(org-level-7 ((t (:inherit outline-5 :height 1.1)))))
 
 (require 'org-tempo)
 
@@ -584,6 +584,10 @@
 (use-package toc-org
     :commands toc-org-enable
     :init (add-hook 'org-mode-hook 'toc-org-enable))
+
+(use-package ox-hugo
+  :ensure t   ;Auto-install the package from Melpa
+  :after ox)
 
 (use-package pdf-tools
   :defer t
