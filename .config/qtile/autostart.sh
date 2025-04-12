c03f073c0c107fe751e60cf2f8ce6a825f7515b4
Derek Taylor <derek@distrotube.com>
2025-04-12 18:20:12 -0500

    Getting configs ready for DTOS launch.

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -3,13 +3,22 @@
 COLORSCHEME=DoomOne
 
 ### AUTOSTART PROGRAMS ###
-lxsession &
-picom --daemon &
-/usr/bin/emacs --daemon &
+
+if systemd-detect-virt --quiet; then
+    lxsession &
+    sleep 1
+    killall picom
+    xrandr -s 1920x1080 &
+else
+    lxsession &
+fi
+
 nm-applet &
 "$HOME"/.screenlayout/layout.sh &
 sleep 1
 conky -c "$HOME"/.config/conky/qtile/01/"$COLORSCHEME".conf || echo "Couldn't start conky."
+sleep 1
+/usr/bin/emacs --daemon &
 
 ### UNCOMMENT ONLY ONE OF THE FOLLOWING THREE OPTIONS! ###
 # 1. Uncomment to restore last saved wallpaper
@@ -18,3 +27,7 @@ xargs xwallpaper --stretch < ~/.cache/wall &
 # find /usr/share/backgrounds/dtos-backgrounds/ -type f | shuf -n 1 | xargs xwallpaper --stretch &
 # 3. Uncomment to set wallpaper with nitrogen
 # nitrogen --restore &
+
+if  [ ! -d "$HOME"/.cache/betterlockscreen/ ]; then
+    betterlockscreen -u /usr/share/backgrounds/dtos-backgrounds/0277.jpg & 
+fi
