f831d288efecfef9e3f5885d54225716c442b7b5
Derek Taylor <derek@distrotube.com>
2025-05-02 17:00:38 -0500

    Adding waypaper to qtile config for a wallpaper changer.

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -22,12 +22,14 @@ sleep 1
 yes | /usr/bin/emacs --daemon &
 
 ### UNCOMMENT ONLY ONE OF THE FOLLOWING THREE OPTIONS! ###
-# 1. Uncomment to restore last saved wallpaper
-xargs xwallpaper --stretch < ~/.cache/wall &
-# 2. Uncomment to set a random wallpaper on login
+# 1. Uncomment to set wallpaper with 'waypaper' 
+waypaper --restore &
+
+# 2. Uncomment to set wallpaper with 'dm-setbg' from dmscripts
+# xargs xwallpaper --stretch < ~/.cache/wall &
+
+# 3. Uncomment to set a random wallpaper on login
 # find /usr/share/backgrounds/dtos-backgrounds/ -type f | shuf -n 1 | xargs xwallpaper --stretch &
-# 3. Uncomment to set wallpaper with nitrogen
-# nitrogen --restore &
 
 if  [ ! -d "$HOME"/.cache/betterlockscreen/ ]; then
     betterlockscreen -u /usr/share/backgrounds/dtos-backgrounds/0277.jpg & 
