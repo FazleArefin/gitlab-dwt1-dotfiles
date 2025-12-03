777960ac7b73ba9a72f8cf86a97c4b0f3a6cfd27
Derek Taylor <derek@distrotube.com>
2025-12-03 11:18:49 -0600

    Update

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -4,23 +4,17 @@ COLORSCHEME=doom-one
 
 ### AUTOSTART PROGRAMS ###
 
-if systemd-detect-virt --quiet; then
-    lxsession &
-    sleep 1
-    killall picom
-    xrandr -s 1920x1080 &
-else
-    lxsession &
-fi
-
+picom --daemon &
 dunst -conf "$HOME"/.config/dunst/"$COLORSCHEME" &
 nm-applet &
-systemctl --user start mpd &
+/usr/bin/emacs --daemon &
 "$HOME"/.screenlayout/layout.sh &
+
+#systemctl --user start mpd &
+
 sleep 1
 conky -c "$HOME"/.config/conky/qtile/01/"$COLORSCHEME".conf || echo "Couldn't start conky."
 sleep 1
-yes | /usr/bin/emacs --daemon &
 
 ### UNCOMMENT ONLY ONE OF THE FOLLOWING THREE OPTIONS! ###
 # 1. Uncomment to set wallpaper with 'waypaper' 
